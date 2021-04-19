77c3fdb1456408ab684582177a5baf116cce882a
Tree <56370416+Tojo-hung@users.noreply.github.com>
2021-04-18 21:17:53 -0700

    Add files via upload

---
+++ import pyautogui.py
@@ -0,0 +1,13 @@
+import pyautogui
+from time import sleep
+
+
+pyautogui.click(1700,1005) #moves the cursor and clicks to any location on the screem
+
+def write(n):
+    for i in range(n):
+        pyautogui.write('boo')   # writes text into chat
+        pyautogui.press('enter') # enter to send to chat
+
+write(69)
+
