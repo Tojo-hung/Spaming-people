01232c01a0d01ca123d28a3383b214790aac8475
scord <70288480+scordd@users.noreply.github.com>
2021-04-20 11:16:09 -0700

    Just made made ur code better xd
    get gud kid

--- spam.py
+++ spam.py
@@ -1,14 +1,18 @@
+from win32api import GetSystemMetrics
 import pyautogui
 from time import sleep
 
+width = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
 
-pyautogui.click(1700,1005) #moves the cursor and clicks to any location on the screem
+pyautogui.click(width / 2, height / 2) #moves the cursor and clicks to any location on the screem
 
 def write(n):
     for i in range(n):
-        pyautogui.write('boo')   # writes text into chat
+        pyautogui.press('tab')
+        pyautogui.write('XDbrozos')   # writes text into chat
         pyautogui.press('enter') # enter to send to chat
         sleep(2)
 
-write(69)
+write(69420)
 
